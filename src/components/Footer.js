5bf96d739eae66c1779f782dad0f3d38ae1e5612
Chen Tao <895433995@qq.com>
2018-12-19 17:01:06 +0800

    第四次修改

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,8 +12,12 @@ class Footer extends Component {
     render() {
         return (
             <footer className="footer">
-                <Link to="vol"><div className={this.props.on==0?"footer-on-color":""}>{this.props.footerTitle1}</div></Link>
-                <Link to=""><div className={this.props.on==1?"footer-on-color":""}>{this.props.footerTitle2}</div></Link>
+                <Link to="vol">
+                    <div className={this.props.on==0?"footer-on-color":""}>{this.props.footerTitle1}</div>
+                </Link>
+                <Link to="">
+                    <div className={this.props.on==1?"footer-on-color":""}>{this.props.footerTitle2}</div>
+                </Link>
             </footer>
         );
     }
